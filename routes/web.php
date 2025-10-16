f38ccf1d70332f32716affd4048a6c30e4629e6d
ArifDoloksaribu <143679155+ArifDoloksaribu@users.noreply.github.com>
2025-10-16 16:37:19 +0700

    controller, DatabaseSeeder dan route yang akan digunakan

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BookController::class, 'index'])->name('home');
+Route::get('/borrow', [BookController::class, 'borrow'])->name('borrow');
+
+// API routes for loans
+Route::post('/api/loans', [BookController::class, 'storeLoan']);
+Route::delete('/api/loans/{id}', [BookController::class, 'returnBook']);
+Route::delete('/api/loans', [BookController::class, 'clearLoans']);
